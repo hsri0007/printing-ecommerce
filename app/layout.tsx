4ce0b765a15f7b4f55314871632805d47e9c2bf2
hsri0007 <hsri0007@gmail.com>
2025-10-31 12:27:53 +0530

    added og tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,28 @@ import WhatsAppButton from "@/components/whats-app"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Brahmani prints - Professional Printing Services",
+  title: "Brahmani Graphics - Professional Printing Services",
   description: "High-quality printing services for all your business and personal needs",
+  openGraph: {
+    title: "Brahmani Graphics | prints - Professional Printing Services",
+    description: "We design logos, business cards, websites & graphics that elevate your brand presence.",
+    url: "https://brahmanigraphics.com",
+    siteName: "Brahmani Graphics",
+    images: [
+      {
+        url: "https://brahmanigraphics.com/brahmani_graphics.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Brahmani Graphics | prints - Professional Printing Services",
+    description: "We design logos, business cards, websites & graphics that elevate your brand presence.",
+    images: ["https://brahmanigraphics.com/brahmani_graphics.png"],
+  },
 }
 
 export default function RootLayout({
